a96b7cc603c6de065a45c2dfb4afa7e6e51531fd
Nikolay Fedurko <32279961+B1Z0N@users.noreply.github.com>
2020-10-16 00:25:39 +0300

    Update factorial.pl

--- src/random/factorial.pl
+++ src/random/factorial.pl
@@ -1,5 +1,7 @@
+%% factorial(+N:int, -R:int) is semidet
 factorial(N, R) :- factorial(N, 1, R).
 
+%% factorial(+N:int, +Acc:int, -R:int) is semidet
 factorial(0, R, R) :- !.
 
 factorial(N, Acc, R) :- 
