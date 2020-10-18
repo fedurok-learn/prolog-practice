b9994a4b522090e32645d50bad28eac785befe48
Nikolay Fedurko <32279961+B1Z0N@users.noreply.github.com>
2020-10-19 00:52:55 +0300

    Update common_descendant.pl

--- src/labs/1/common_descendant.pl
+++ src/labs/1/common_descendant.pl
@@ -4,9 +4,9 @@ parent(john, jacob).
 parent(victor, john).
 parent(olga, loki).
 
-%% desc(-X:atom, -D:atom) is semidet.
+%% desc(-X:atom, -D:atom) is nondet.
 desc(X, D) :- parent(X, D).
 desc(X, D) :- parent(X, XD), desc(XD, D).
 
-%% common_descendant(-X:atom, -Y:atom, -D:atom) is semidet.
+%% common_descendant(-X:atom, -Y:atom, -D:atom) is nondet.
 common_descendant(X, Y, CD) :- desc(X, CD), desc(Y, CD).
