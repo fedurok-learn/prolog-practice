36a77672315f265c5fdbc6aaf2f81acd5edd28a2
Nikolay Fedurko <32279961+B1Z0N@users.noreply.github.com>
2020-10-20 00:51:17 +0300

    Update bf_shortest_path.pl

--- src/random/bf_shortest_path.pl
+++ src/random/bf_shortest_path.pl
@@ -1,5 +1,4 @@
 %% https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Shortest_path_with_direct_weights.svg/1200px-Shortest_path_with_direct_weights.svg.png
-%% this program is to be done in future
 
 distance(a, b, 4). 
 distance(a, c, 2).
@@ -9,9 +8,21 @@ distance(c, e, 3).
 distance(e, d, 4).
 distance(d, f, 11).
 
-shortest_path(X, X, R) :- R = 0.
+%% shortest_path(+A:atom, +B:atom, -R:int) is semidet.
+%% shortest path between nodes in directed graph
+shortest_path(A, B, R) :- 
+    findall(X, path(A, B, [], 0, X), Lst),
+    min_list(Lst, R).
 
-shortest_path(X, Y, R) :-
-    distance(X, Z, D),
-    shortest_path(Z, Y, R2),
-    R is D + R2.
+%% graph_path(+A:atom, +B:atom, +Visited:list, +Acc:int, -R:int) is nondet.
+%% @param A and B - first nodes
+%% @param Visited - list of already visited nodes
+%% @param Acc - accumulator so that we could
+%% incorporate tail call recursion optimisation
+%% @param R - resulting path length
+path(A, A, _Visited, Acc, Acc).
+path(A, B, Visited, Acc, R) :-
+    distance(A, C, AC),
+    \+ member(C, Visited),
+    NewAcc is Acc + AC,
+    path(C, B, [C|Visited], NewAcc, R).
